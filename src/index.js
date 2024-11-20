dd5bb8f14e807d2de6595f51785c6fbe9c2351a3
yuuki-nya <47957260+yuuki-nya@users.noreply.github.com>
2024-11-20 16:21:10 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
+  ["docker-proxy." + CUSTOM_DOMAIN]: dockerHub,
+  ["quay-proxy." + CUSTOM_DOMAIN]: "https://quay.io",
+  ["gcr-proxy." + CUSTOM_DOMAIN]: "https://gcr.io",
+  ["k8s-gcr-proxy." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
+  ["k8s-proxy." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
+  ["ghcr-proxy." + CUSTOM_DOMAIN]: "https://ghcr.io",
+  ["cloudsmith-proxy." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
+  ["ecr-proxy." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
 
   // staging
   ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
